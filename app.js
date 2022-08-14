ae7791b6ed823e4a8b976ac7e609c514d489ec7a
Rana Pratik <ranapratik1612@gmail.com>
2022-08-14 17:57:55 +0530

    started

---
+++ app.js
@@ -0,0 +1,14 @@
+const divs = document.getElementsByName('div');
+window.addEventListener('scroll',sc);
+
+function sc()
+{
+    let winHeight = window.innerHeight/5*4;
+    divs.forEach(div => {
+        let divPosition=div.getBoundingClientRect().top;
+        if(divPosition < winHeight)
+        {
+            div.classList.add('show');
+        }
+    })
+};
