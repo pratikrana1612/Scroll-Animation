d7b125b1d1f0d7c8cd67df9608ccee7ae08b377b
Rana Pratik <ranapratik1612@gmail.com>
2022-08-14 21:14:29 +0530

    Scroll Animation Completed

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const divs = document.querySelectorAll('div');
 
 window.addEventListener('scroll',scrollHandler);
 
+let winHeight = window.innerHeight;
 scrollHandler();
+
 function scrollHandler()
 {
-    let winHeight = window.innerHeight;
     divs.forEach(div => {
         let divPosition=div.getBoundingClientRect().top;
         if(divPosition < winHeight)
